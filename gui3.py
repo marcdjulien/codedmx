b1ecdb805310df100719cf9e54c1952192ff28ac
Marc-Daniel Julien <marcdjulien@gmail.com>
2023-09-19 04:11:52 -0700

    Update gui3.py

--- gui3.py
+++ gui3.py
@@ -31,17 +31,23 @@ class Gui:
         self.gui_state = None
         self.new_gui_state = None
 
+        self._active_output_channel = None
+        self._inspecter_x = list(range(500))
+
     def run(self, state, gui_state):
         self.state = state
         self.gui_state = gui_state
         self.tags = {
             "automation_window": [],
             "node_window": [],
+            "output_configuration_window": [],
             "gui_to_channel": {},
             "channel_to_gui": {},
             "attr_to_channel": {},
             "automation_to_gui": {},
             "playhead_tags": {},
+            "output_config_tag_to_channel": {},
+            "output_node_tag_to_channel": {}
         }
         print("Create Context")
         dpg.create_context()
@@ -69,7 +75,7 @@ class Gui:
                             "clip_i": clip_i,
                             "action": "create" if clip is None else "restore"
                         },
-                        tag=f"clip[{track_i},{clip_i}]",
+                        tag=f"track[{track_i}].clip[{clip_i}]",
                     )
                     with dpg.popup(dpg.last_item()):
                         dpg.add_menu_item(label="Save As", callback=self.print_callback)
@@ -88,12 +94,33 @@ class Gui:
                             "action": "create" if clip is None else "restore"
                         })    
 
+                track_title_tag = f"track[{track_i}].title"
                 dpg.add_text(
                     default_value=self.state.tracks[track_i].name,
                     pos=((w+x_sep) * track_i + x_offset, y_offset),
+                    tag=track_title_tag
                 )
-                with dpg.popup(dpg.last_item()):
-                    dpg.add_menu_item(label="Save As", callback=self.print_callback)
+                with dpg.popup(track_title_tag, tag=f"track[{track_i}].popup"):
+                    dpg.add_menu_item(label="Rename", callback=self.print_callback)
+
+
+                # Create Output Configuration Window
+                output_configuration_window_tag = f"track[{track_i}].output_configuration_window"
+                self.create_output_configuration_window(output_configuration_window_tag, track_i)
+
+                # When user clicks on the track title, bring up the output configuration window.
+                def open_output_configuration_window(sender, app_data, user_data):
+                    if app_data[0] == 0:
+                        for tag in self.tags["output_configuration_window"]:
+                            dpg.configure_item(tag, show=False)
+                        dpg.configure_item(user_data, show=True)
+                    else:
+                        dpg.configure_item(f"track[{track_i}].popup", show=True)
+
+                handler_registry_tag = f"{track_title_tag}.item_handler_registry"
+                with dpg.item_handler_registry(tag=handler_registry_tag) as handler:
+                    dpg.add_item_clicked_handler(callback=open_output_configuration_window, user_data=output_configuration_window_tag)
+                dpg.bind_item_handler_registry(track_title_tag, handler_registry_tag)
 
         #### Mouse Handlers ####
         with dpg.handler_registry():
@@ -179,6 +206,8 @@ class Gui:
             dpg.add_text("Tempo:", pos=(transport_start_x + 90,0))
             dpg.add_input_text(label="Tempo", default_value=self.state.tempo, pos=(transport_start_x + 130, 0), on_enter=True, decimal=True, callback=update_tempo)
 
+        self.create_inspector_window()
+
         dpg.setup_dearpygui()
         dpg.show_viewport()
 
@@ -204,7 +233,8 @@ class Gui:
         return self.new_state, self.new_gui_state
 
     def create_new_node_editor(self, parent, state, track_i, clip_i):
-        clip = state.tracks[track_i][clip_i]
+        track = state.tracks[track_i]
+        clip = track.clips[clip_i]
 
         with dpg.window(
             tag=parent,
@@ -218,14 +248,14 @@ class Gui:
                 action = user_data[0]
                 if action == "create":
                     new_clip = state.tracks[track_i][clip_i]
-                    input_index = len(new_clip.node_collection.inputs)
-                    state.execute(f"create_input clip[{track_i},{clip_i}]")
+                    input_index = len(new_clip.inputs)
+                    state.execute(f"create_input track[{track_i}].clip[{clip_i}]")
                 else: # restore
                     _, new_clip, input_index = user_data
 
-                input_channel = new_clip.node_collection.inputs[input_index]
+                input_channel = new_clip.inputs[input_index]
     
-                clip_input_id = f"clip[{track_i},{clip_i}].clip.in[{input_index}]"
+                clip_input_id = f"track[{track_i}].clip[{clip_i}].in[{input_index}]"
                 node_tag = f"{clip_input_id}.node"
                 with dpg.node(label=f"Input {input_index}", tag=node_tag, parent=f"{parent}.node_editor"):
                     attr_tag = f"{clip_input_id}"
@@ -259,41 +289,15 @@ class Gui:
                         dpg.add_item_clicked_handler(callback=open_automation_window, user_data=automation_window_tag)
                     dpg.bind_item_handler_registry(node_tag, handler_registry_tag)
 
-            def add_output(sender, app_data, user_data):
-                action = user_data[0]
-                if action == "create":
-                    new_clip = state.tracks[track_i][clip_i]
-                    output_index = len(new_clip.node_collection.outputs)
-                    state.execute(f"create_output clip[{track_i},{clip_i}]")
-                else: # restore
-                    _, new_clip, output_index = user_data
-
-                output_channel = new_clip.node_collection.outputs[output_index]
-
-                clip_output_id = f"clip[{track_i},{clip_i}].clip.out[{output_index}]"
-                node_tag = f"{clip_output_id}.node"
-                with dpg.node(label=f"Output {output_index}", tag=node_tag, parent=f"{parent}.node_editor"):
-                    attr_tag = f"{clip_output_id}"
-                    self.tags["attr_to_channel"][attr_tag] = output_channel
-                    with dpg.node_attribute(tag=f"{attr_tag}"):
-                        value_tag = f"{attr_tag}.value"
-                        dpg.add_input_int(label="In", tag=f"{value_tag}", width=75, readonly=True)
-                        self.tags["channel_to_gui"][value_tag] = output_channel
-
-                    with dpg.node_attribute(tag=f"{attr_tag}.dmx_channel", attribute_type=dpg.mvNode_Attr_Static):
-                        value_tag = f"{attr_tag}.dmx_channel.value"
-                        dpg.add_input_int(label="Ch.", tag=f"{value_tag}", width=75, )
-                        #self.tags["channel_to_gui"][value_tag] = output_channel
-
-                # TODO: Add output inspecter
 
             def add_function_node(sender, app_data, user_data):
                 action = user_data[0]
                 if action == "create":
                     node_type = user_data[1]
+                    track = state.tracks[track_i]
                     new_clip = state.tracks[track_i][clip_i]
                     node_index = len(new_clip.node_collection.nodes)
-                    state.execute(f"create_node clip[{track_i},{clip_i}] {node_type}")
+                    state.execute(f"create_node track[{track_i}].clip[{clip_i}] {node_type}")
                 else: # restore
                     _, new_clip, node_index = user_data
                     node_type = new_clip.node_collection.nodes[node_index].type
@@ -301,7 +305,7 @@ class Gui:
                 input_channels = new_clip.node_collection.nodes[node_index].inputs
                 output_channels = new_clip.node_collection.nodes[node_index].outputs
 
-                clip_node_id = f"clip[{track_i},{clip_i}].node[{node_index}]"
+                clip_node_id = f"track[{track_i}].clip[{clip_i}].node[{node_index}]"
                 node_tag = f"{clip_node_id}.node"
                 with dpg.node(parent=f"{parent}.node_editor", tag=node_tag):
                     if node_type == "add_node":
@@ -336,8 +340,8 @@ class Gui:
 
                 if action == "create":
                     src = dpg.get_item_alias(app_data[0])
-                    dst = dpg.get_item_alias(app_data[1])
-                    state.execute(f"create_link {src} {dst}")
+                    dst = dpg.get_item_alias(app_data[1]).split("-")[0]
+                    state.execute(f"create_link track[{track_i}].clip[{clip_i}] {src} {dst}")
                 else: # restore
                     _, src_channel, dst_channel = user_data
                     src, dst = None, None
@@ -351,8 +355,7 @@ class Gui:
                     print(src, dst)
                     assert src and dst
 
-                link_tag = f"{src}:{dst}"
-                dpg.add_node_link(src, dst, parent=f"{parent}.node_editor", tag=link_tag)
+                dpg.add_node_link(app_data[0], app_data[1], parent=f"{parent}.node_editor")
 
             def delete_link(sender, app_data):
                 dpg.delete_item(app_data)
@@ -373,22 +376,23 @@ class Gui:
                         label="Add", user_data=("create", "add_node"), callback=add_function_node
                     )
 
-                dpg.add_menu_item(label="Create Output", callback=add_output, user_data=("create",))
 
         ###############
         ### Restore ###
         ###############
-        for input_index, input_channel in enumerate(clip.node_collection.inputs):
+        for input_index, input_channel in enumerate(clip.inputs):
             add_input(sender=None, app_data=None, user_data=("restore", clip, input_index))
 
-        for output_index, output_channel in enumerate(clip.node_collection.outputs):
-            add_output(sender=None, app_data=None, user_data=("restore", clip, output_index))
+        for output_index in range(len(track.outputs)):
+            self.add_output_node_gui(track_i, clip_i, output_index)
 
         for node_index, node_channel in enumerate(clip.node_collection.nodes):
             add_function_node(sender=None, app_data=None, user_data=("restore", clip, node_index))
 
         for link_index, link in enumerate(clip.node_collection.links):
             add_link(sender=None, app_data=None, user_data=("restore", link.src_channel, link.dst_channel))
+
+
 
     def create_new_automation_editor(self, parent, state, track_i, clip_i, input_index):
         clip = state.tracks[track_i][clip_i]
@@ -400,10 +404,10 @@ class Gui:
             height=600,
             pos=(810, 0),
         ) as window:
-            channel = clip.node_collection.inputs[input_index]
+            channel = clip.inputs[input_index]
             automation = clip.automation_map[channel]
 
-            clip_input_id = f"clip[{track_i},{clip_i}].clip.in[{input_index}]"
+            clip_input_id = f"track[{track_i}].clip[{clip_i}].in[{input_index}]"
             series_tag = f"{clip_input_id}.series"
             plot_tag = f"{clip_input_id}.plot"
             playhead_tag = f"{clip_input_id}.playhead"
@@ -486,16 +490,132 @@ class Gui:
             self.tags["automation_to_gui"][series_tag] = automation
             self.tags["playhead_tags"][playhead_tag] = (state, clip, automation)
 
+    def create_inspector_window(self):
+        with dpg.window(
+            label=f"Inspector",
+            width=750,
+            height=600,
+            pos=(810, 0),
+        ) as window:
+            with dpg.plot(label="Inspector", height=-1, width=-1):
+                dpg.add_plot_axis(dpg.mvXAxis, label="x")
+                dpg.set_axis_limits(dpg.last_item(), 0, len(self._inspecter_x))
+
+                dpg.add_plot_axis(dpg.mvYAxis, label="y")
+                dpg.set_axis_limits(dpg.last_item(), 0, 255)
+                dpg.add_line_series(
+                    [],
+                    [],
+                    tag="inspector.series",
+                    parent=dpg.last_item(),
+                )
+
+
+    def create_output_configuration_window(self, parent, track_i):
+        track = self.state.tracks[track_i]
+        with dpg.window(
+            tag=parent,
+            label=f"Output Configuration | {track.name}",
+            width=250,
+            height=600,
+            pos=(200, 200),
+            show=False
+        ) as window:
+            output_table_tag = f"{parent}.output_table"
+
+            def add_output(sender, app_data, user_data):
+                action = user_data[0]
+                if action == "create":
+                    track = state.tracks[track_i]
+                    output_index = len(track.outputs)
+                    state.execute(f"create_output {track_i}")
+                else: # restore
+                    _, track, output_index = user_data
+
+                output_channel = track.outputs[output_index]
+
+                output_table_row_tag = f"{output_table_tag}.row[{output_index}]"
+                with dpg.table_row(parent=output_table_tag):
+                    dmx_channel_tag = f"{output_table_row_tag}.dmx_channel"
+                    dpg.add_input_int(label="DMX Channel", tag=dmx_channel_tag, width=75, )
+                    self.tags["output_config_tag_to_channel"][dmx_channel_tag] = output_channel
+
+                # Add a Node to each clip's node editor
+                for clip_i, clip in enumerate(track.clips):
+                    if clip is None:
+                        continue                
+                    self.add_output_node_gui(track_i, clip_i, output_index)
+
+            dpg.add_button(
+                label="Add Output",
+                callback=add_output,
+                user_data=("create",)
+            )
+
+            with dpg.table(header_row=True, tag=output_table_tag):
+                dpg.add_table_column(label="Outputs", tag=f"{output_table_tag}.column")
+
+            self.tags["output_configuration_window"].append(parent)
+
+        ###############
+        ### Restore ###
+        ###############
+        for output_index, output_channel in enumerate(track.outputs):
+            add_output(sender=None, app_data=None, user_data=("restore", track, output_index))
+
+    def add_output_node_gui(self, track_i, clip_i, output_index):
+        track = self.state.tracks[track_i]
+        output_channel = track.outputs[output_index]
+
+        parent = f"node_window[{track_i},{clip_i}]"
+        # This is the id used when adding links.
+        track_output_id = f"track[{track_i}].out[{output_index}]-{clip_i}"
+        node_tag = f"{track_output_id}.{clip_i}.node"
+
+        if dpg.does_item_exist(node_tag):
+            return
+        
+        with dpg.node(label=f"Output {output_index}", tag=node_tag, parent=f"{parent}.node_editor"):
+            attr_tag = f"{track_output_id}"
+            self.tags["attr_to_channel"][attr_tag] = output_channel
+            with dpg.node_attribute(tag=f"{attr_tag}"):
+                value_tag = f"{attr_tag}.value"
+                dpg.add_input_int(label="In", tag=f"{value_tag}", width=75, readonly=True)
+                self.tags["channel_to_gui"][value_tag] = output_channel
+
+            with dpg.node_attribute(tag=f"{attr_tag}.dmx_channel", attribute_type=dpg.mvNode_Attr_Static):
+                value_tag = f"{attr_tag}.dmx_channel.value"
+                dpg.add_text(tag=f"{value_tag}", default_value=f"DMX Ch. {output_channel.dmx_channel}")
+                self.tags["output_node_tag_to_channel"][value_tag] = output_channel
+
+            # When user clicks on the output node it will populate the inspector.
+            def set_inspector(sender, app_data, user_data):
+                self._active_output_channel = user_data
+
+            handler_registry_tag = f"{node_tag}.item_handler_registry"
+            with dpg.item_handler_registry(tag=handler_registry_tag) as handler:
+                dpg.add_item_clicked_handler(callback=set_inspector, user_data=output_channel)
+            dpg.bind_item_handler_registry(node_tag, handler_registry_tag)
+
+
+        # TODO: Add output inspecter
+
     def update_state_from_gui(self):
         # Update Input Channels from GUI value
         for tag, channel in self.tags["gui_to_channel"].items():
             channel.set(dpg.get_value(tag))
+
+        for tag, channel in self.tags["output_config_tag_to_channel"].items():
+            channel.dmx_channel = dpg.get_value(tag)
 
 
     def update_gui_from_state(self):
         # Update nodes from Channel values
         for tag, channel in self.tags["channel_to_gui"].items():
             dpg.set_value(tag, channel.get())
+
+        for tag, channel in self.tags["output_node_tag_to_channel"].items():
+            dpg.set_value(tag, f"DMX Ch. {channel.dmx_channel}")
 
         # Update automation points
         for tag, automation in self.tags["automation_to_gui"].items():
@@ -508,6 +628,14 @@ class Gui:
                 x=[x[0] for x in values if x[0] is not None],
                 y=[x[1] for x in values if x[0] is not None],
             )
+
+        # Update Inspector
+        if self._active_output_channel is not None:
+            dpg.configure_item(
+                    "inspector.series",
+                    x=self._inspecter_x,
+                    y=self._active_output_channel.history[-1 - len(self._inspecter_x):-1],
+                )
 
         # Set the play heads to the correct position
         for playhead_tag, (state, clip, automation) in self.tags["playhead_tags"].items():
@@ -548,17 +676,17 @@ class Gui:
         mouse_pos = dpg.get_mouse_pos()
         plot_mouse_pos = dpg.get_plot_mouse_pos()
         if window_tag is not None and window_tag.endswith("automation_window"):
-            match = re.match(r"clip\[(\d+),(\d+)\]\.clip\.in\[(\d+)\]\.automation_window", window_tag)
+            match = re.match(r"track\[(\d+)\]\.clip\[(\d+)\]\.in\[(\d+)\]\.automation_window", window_tag)
             if match:
                 track_i, clip_i, input_index = match.groups()
-                channel = state.tracks[int(track_i)][int(clip_i)].node_collection.inputs[
+                channel = state.tracks[int(track_i)][int(clip_i)].inputs[
                     int(input_index)
                 ]
                 automation = state.tracks[int(track_i)][int(clip_i)].automation_map[
                     channel
                 ]
 
-                clip_input_id = f"clip[{track_i},{clip_i}].clip.in[{input_index}]"
+                clip_input_id = f"track[{track_i}].clip[{clip_i}].in[{input_index}]"
 
                 for i, x in enumerate(automation.values_x):
                     if x is None:
@@ -609,7 +737,7 @@ class Gui:
         point_id, point_index = tag.split(".series.")
 
         if point_index in ["0", "1"]:
-            channel = state.tracks[track_i][clip_i].node_collection.inputs[input_index]
+            channel = state.tracks[track_i][clip_i].inputs[input_index]
             automation = state.tracks[track_i][clip_i].automation_map[channel]
             original_x = automation.values_x[int(point_index)]
             dpg.set_value(sender, (original_x, y))
