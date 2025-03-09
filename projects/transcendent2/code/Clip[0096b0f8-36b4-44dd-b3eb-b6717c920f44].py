39185b5272be9413ddecf105e4835b81ba2458c7
MDJ <marcdjulien@gmail.com>
2025-03-08 18:59:28 -0800

    Remove projects

--- projects/transcendent2/code/Clip[0096b0f8-36b4-44dd-b3eb-b6717c920f44].py
+++
@@ -1,8 +0,0 @@
-all_pars = [Par1, Par2, Par3, Par4]
-rate = Scale(Rate.value, 0, 100, 0.025, 0.5)
-i = Sample(rate, Random(1, 4), key="random_par")
-Demux(i, 255, [Par.dimmer for Par in all_pars])
-color = GlobalStorage.get(f"Color{ColorSelect.value}")
-
-for i in range(4):
-    set_par_color(all_pars[i], color)
--- projects/transcendent2/code/Track[8866d4bf-0d07-4151-996b-1f20468898e0].py
+++
@@ -1,5 +0,0 @@
-global_dimmer = GlobalStorage.get("GlobalDimmer").value
-
-adj_dimmer = GlobalStorage.get("ADJDimmer").value
-ADJ1.dimmer.value = NormMult([ADJ1.dimmer.value, adj_dimmer, global_dimmer], 255)
-ADJ2.dimmer.value = NormMult([ADJ2.dimmer.value, adj_dimmer, global_dimmer], 255)
